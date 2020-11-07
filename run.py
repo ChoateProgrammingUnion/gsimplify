df915392849f3253072adcf4ee3cfe331d771480
Max Fan <theinnovativeinventor@gmail.com>
2020-11-07 16:24:50 -0500

    Added drive datatypes with pydantic!

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ import simplify
 if __name__ == "__main__":
     service = simplify.auth.load_drive()
     drive = simplify.drive.Drive(drive_id ='0AFB5gF1TxS1vUk9PVA', service =service)
-    print(drive.files())
+    print(drive.docs())
--- simplify/drive.py
+++ simplify/drive.py
@@ -2,19 +2,49 @@ from google.auth.transport.requests import Request
 from google.oauth2 import service_account
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import Resource, build
+from pydantic import BaseModel
 
+
+class DriveObj(BaseModel):
+    name: str
+    id: str
+    kind: str
+    mime_type: str
+    drive_id: str
+
+class Doc(DriveObj):
+    pass
+
+class Folder(DriveObj):
+    pass
 
 class Drive:
     def __init__(self, drive_id: str, service: Resource):
         self.drive_id = drive_id
         self.drive_info = service.teamdrives().get(teamDriveId=drive_id).execute()
         self.service = service
+        self.items = self.contains()
         
-    def files(self):
-        return self.service.files().list(
+    def contains(self):
+        drive_obj = self.service.files().list(
             corpora='teamDrive',
             supportsTeamDrives=True,
             teamDriveId=self.drive_id,
             includeTeamDriveItems=True
-        ).execute()
+        ).execute().get("files")
 
+        container = []
+        for each_obj in drive_obj:
+            if each_obj.get('mimeType') == "application/vnd.google-apps.document":
+                container.append(Doc(kind=each_obj.get('kind'), id=each_obj.get('id'), name=each_obj.get('name'), mime_type = each_obj.get('mimeType'), drive_id = each_obj.get('driveId')))
+
+            elif each_obj.get('kind') == "application/vnd.google-apps.folder":
+                container.append(Doc(kind=each_obj.get('kind'), id=each_obj.get('id'), name=each_obj.get('name'), mime_type = each_obj.get('mimeType'), drive_id = each_obj.get('driveId')))
+
+        return container
+
+    def docs(self):
+        return [x for x in self.items if isinstance(x, Doc)]
+
+    def folders(self):
+        return [x for x in self.items if isinstance(x, Folder)]
